c820bf09d45dbf4e79ff88e6acc6cb006a999e36
dizzzz@gmail.com <dizzzz@gmail.com@66bb6a36-0227-0410-b297-1b65bf2f2943>
2010-06-26 17:51:51 +0000

    another step, close to finish

--- asocat/src/nl/ow/dilemma/ant/fetch/FetchTask.java
+++ asocat/src/nl/ow/dilemma/ant/fetch/FetchTask.java
@@ -7,13 +7,18 @@ package nl.ow.dilemma.ant.fetch;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Available;
 import org.apache.tools.ant.taskdefs.Get;
 import org.apache.tools.ant.taskdefs.Expand;
 import org.apache.tools.ant.types.Mapper;
+import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.PatternSet;
+import org.apache.tools.ant.types.Reference;
 import org.apache.tools.ant.util.FlatFileNameMapper;
 
 /**
@@ -26,34 +31,44 @@ public class FetchTask extends Task {
     private Expand expandTask;
     private URL downloadUrl;
     private File destination;
-    private Project localProject;
+//    private Project localProject;
     private String className;
+    private Path classpath;
 
     public FetchTask() {
+        super();
         getTask = new Get();
         expandTask = new Expand();
     }
 
-    @Override
-    public void setProject(Project project) {
-        localProject = project;
-    }
+//    @Override
+//    public void setProject(Project project) {
+//        localProject = project;
+//    }
 
     @Override
     public void execute() throws BuildException {
-//        super.execute();
 
         if (className != null) {
-            try {
-                Class.forName(className);
+//            try {
+//                getProject().getCoreLoader().loadClass(className);
+////                Class.forName(className);
 //                log("Class found, skipping");
-
+//
+//                return;
+//            } catch (ClassNotFoundException exception) {
+//                log("Class not found found "+className);
+//            }
+            Available av = new Available();
+            av.setClassname(className);
+            av.setProject(getProject());
+            av.setClasspath(classpath);
+            if(av.eval()){
+                log("Class "+className+" is already present, skipping download.");
                 return;
-            } catch (ClassNotFoundException exception) {
-                // ignore
             }
-        }
 
+        }
 
         File tmpFile = null;
         try {
@@ -74,11 +89,12 @@ public class FetchTask extends Task {
 
     private void getFromUrl(File tmp) throws BuildException {
         try {
-            getTask.setProject(localProject);
+            getTask.setProject(getProject());
             getTask.setSrc(downloadUrl);
             getTask.setDest(tmp);
             getTask.setVerbose(true);
             getTask.setRetries(5);
+            getTask.setTaskName(getTaskName());
             getTask.execute();
 
         } catch (Exception ex) {
@@ -105,16 +121,65 @@ public class FetchTask extends Task {
     private void unzipToDirectory(File tmp) {
 
         if (!destination.exists()) {
+            log("Creating "+getCanonicalPath(destination));
             destination.mkdirs();
         }
 
-        expandTask.setProject(localProject);
+        expandTask.setProject(getProject());
         expandTask.setSrc(tmp);
-        expandTask.setDest(destination);
+        expandTask.setDest(getCanonicalFile(destination));
+        expandTask.setTaskName(getTaskName());
 
         Mapper m = expandTask.createMapper();
         m.add(new FlatFileNameMapper());
 
         expandTask.execute();
+    }
+
+    private String getCanonicalPath(File in){
+        try {
+            return in.getCanonicalPath();
+        } catch (IOException ex) {
+            return in.getAbsolutePath();
+        }
+    }
+
+    private File getCanonicalFile(File in){
+        try {
+            return in.getCanonicalFile();
+        } catch (IOException ex) {
+            return in.getAbsoluteFile();
+        }
+    }
+
+    /**
+     * Set the classpath to be used when searching for classes and resources.
+     *
+     * @param classpath an Ant Path object containing the search path.
+     */
+    public void setClasspath(Path classpath) {
+        createClasspath().append(classpath);
+    }
+
+    /**
+     * Classpath to be used when searching for classes and resources.
+     *
+     * @return an empty Path instance to be configured by Ant.
+     */
+    public Path createClasspath() {
+        if (this.classpath == null) {
+            this.classpath = new Path(getProject());
+        }
+        return this.classpath.createPath();
+    }
+
+    /**
+     * Set the classpath by reference.
+     *
+     * @param r a Reference to a Path instance to be used as the classpath
+     *          value.
+     */
+    public void setClasspathRef(Reference r) {
+        createClasspath().setRefid(r);
     }
 }
