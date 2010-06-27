e9000a8e419d8a5d9c270babc09e15b011544c65
dizzzz@gmail.com <dizzzz@gmail.com@66bb6a36-0227-0410-b297-1b65bf2f2943>
2010-06-27 09:36:41 +0000

    another step, close to finish. Patterns are not assed to unzip taks yet.

--- trunk/asocat/src/nl/ow/dilemma/ant/fetch/FetchTask.java
+++ trunk/asocat/src/nl/ow/dilemma/ant/fetch/FetchTask.java
@@ -7,10 +7,8 @@ package nl.ow.dilemma.ant.fetch;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Vector;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.Available;
 import org.apache.tools.ant.taskdefs.Get;
@@ -27,55 +25,39 @@ import org.apache.tools.ant.util.FlatFileNameMapper;
  */
 public class FetchTask extends Task {
 
-    private Get getTask;
-    private Expand expandTask;
+//    private Expand expandTask;
     private URL downloadUrl;
     private File destination;
-//    private Project localProject;
     private String className;
     private Path classpath;
-
-    public FetchTask() {
-        super();
-        getTask = new Get();
-        expandTask = new Expand();
-    }
-
-//    @Override
-//    public void setProject(Project project) {
-//        localProject = project;
-//    }
+    private PatternSet ps = new PatternSet();
 
     @Override
     public void execute() throws BuildException {
 
+
+        // If class name is set, check availability class
         if (className != null) {
-//            try {
-//                getProject().getCoreLoader().loadClass(className);
-////                Class.forName(className);
-//                log("Class found, skipping");
-//
-//                return;
-//            } catch (ClassNotFoundException exception) {
-//                log("Class not found found "+className);
-//            }
+
+            // Setup taskdef Available
             Available av = new Available();
             av.setClassname(className);
             av.setProject(getProject());
             av.setClasspath(classpath);
-            if(av.eval()){
-                log("Class "+className+" is already present, skipping download.");
+
+            // Perform actual check
+            if (av.eval()) {
+                log("Class " + className + " is already present, skipping download.");
                 return;
             }
-
         }
 
+        // Execute get and unzip
         File tmpFile = null;
         try {
             tmpFile = File.createTempFile("FetchTask", "tmp");
-            getFromUrl(tmpFile);
-
-            unzipToDirectory(tmpFile);
+            getFromUrl(downloadUrl, tmpFile);
+            unzipToDirectory(tmpFile, destination, ps);
 
         } catch (IOException ex) {
             throw new BuildException(ex);
@@ -87,56 +69,82 @@ public class FetchTask extends Task {
         }
     }
 
-    private void getFromUrl(File tmp) throws BuildException {
+    /**
+     *  Download file from URL using GET task ant
+     *
+     * @param url   URL of to-be-downloaded file
+     * @param tmp   Store Location for downloaded file
+     * @throws BuildException Thrown when something wrong happens.
+     */
+    private void getFromUrl(URL url, File tmp) throws BuildException {
         try {
+            Get getTask = new Get();
             getTask.setProject(getProject());
-            getTask.setSrc(downloadUrl);
+            getTask.setSrc(url);
             getTask.setDest(tmp);
             getTask.setVerbose(true);
             getTask.setRetries(5);
             getTask.setTaskName(getTaskName());
             getTask.execute();
 
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
             throw new BuildException(ex);
         }
+    }
+
+    /**
+     *  Unzip file to specified directory
+     *
+     * @param zipFile   Zip file
+     * @param unzipDirectory  Location to unzip files in.
+     */
+    private void unzipToDirectory(File zipFile, File unzipDirectory, PatternSet set) throws BuildException {
+
+        if (unzipDirectory.exists() && !unzipDirectory.isDirectory()) {
+            throw new BuildException(unzipDirectory.getAbsolutePath() + "already exists and is not an directory.");
+        }
+
+        if (!unzipDirectory.exists()) {
+            log("Creating " + getCanonicalPath(unzipDirectory));
+            unzipDirectory.mkdirs();
+        }
+
+        Expand expandTask = new Expand();
+        expandTask.setProject(getProject());
+        expandTask.setSrc(zipFile);
+        expandTask.setDest(getCanonicalFile(unzipDirectory));
+        expandTask.setTaskName(getTaskName());
+        expandTask.addPatternset(set);
+
+        //Note that the file patterns are already registered to the expand task.
+        Mapper m = expandTask.createMapper();
+        m.add(new FlatFileNameMapper());
+
+        // perform execution
+        expandTask.execute();
     }
 
     public void setUrl(URL u) {
         downloadUrl = u;
     }
 
-    public void setDest(String d) {
-        destination = new File(d);
+    public void setDest(File d) {
+        destination = d;
     }
 
     public void addPatternset(PatternSet set) {
-        expandTask.addPatternset(set);
+        ps.append(set, getProject());
     }
 
     public void setClassname(String n) {
         this.className = n;
     }
 
-    private void unzipToDirectory(File tmp) {
-
-        if (!destination.exists()) {
-            log("Creating "+getCanonicalPath(destination));
-            destination.mkdirs();
-        }
-
-        expandTask.setProject(getProject());
-        expandTask.setSrc(tmp);
-        expandTask.setDest(getCanonicalFile(destination));
-        expandTask.setTaskName(getTaskName());
-
-        Mapper m = expandTask.createMapper();
-        m.add(new FlatFileNameMapper());
-
-        expandTask.execute();
-    }
-
-    private String getCanonicalPath(File in){
+    /**
+     *  Helper method to determine the File's canonicicak path. If conversion
+     * fails the absolute path is returned.
+     */
+    private String getCanonicalPath(File in) {
         try {
             return in.getCanonicalPath();
         } catch (IOException ex) {
@@ -144,7 +152,11 @@ public class FetchTask extends Task {
         }
     }
 
-    private File getCanonicalFile(File in){
+    /**
+     *  Helper method to determine the File's canonicicak file. If conversion
+     * fails the absolute file is returned.
+     */
+    private File getCanonicalFile(File in) {
         try {
             return in.getCanonicalFile();
         } catch (IOException ex) {
@@ -176,10 +188,10 @@ public class FetchTask extends Task {
     /**
      * Set the classpath by reference.
      *
-     * @param r a Reference to a Path instance to be used as the classpath
+     * @param reference a Reference to a Path instance to be used as the classpath
      *          value.
      */
-    public void setClasspathRef(Reference r) {
-        createClasspath().setRefid(r);
+    public void setClasspathRef(Reference reference) {
+        createClasspath().setRefid(reference);
     }
 }
